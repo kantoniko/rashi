d9a6f9e6674f759cd6442f956730e461d94e7b3a
Gabor Szabo <gabor@szabgab.com>
2023-09-27 12:38:31 +0300

    start using jinja template

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import yaml
 import os
+from jinja2 import Environment, FileSystemLoader
 
 
 letters = [
@@ -40,24 +41,18 @@ letters = [
     'שﬞ',
     ]
 
-def main():
-    with open('templates/header.html') as fh:
-        header = fh.read()
-    with open('templates/footer.html') as fh:
-        footer = fh.read()
+html_path = "_site"
 
+def main():
 
     buttons = ""
     for letter in letters:
         buttons += f"""<button class="button letter rashi">{letter}</button>"""
-    footer = footer.replace("BUTTONS", buttons)
 
-    os.makedirs("_site", exist_ok=True)
-    with open("_site/index.html", "w") as fh:
-        fh.write(header)
-        #html = create_table()
-        #fh.write(html)
-        fh.write(footer)
+    render("index.html", "index.html", buttons=buttons)
+
+    #    #html = create_table()
+    #    #fh.write(html)
 
 def create_table():
     file = 'words_2.yaml'
@@ -74,5 +69,20 @@ def create_table():
         html += f"""<tr><td><a href="https://kantoniko.com/words/ladino/{word['latin']}">{word['latin']}</a></td><td class="rashi" dir="rtl">{word['rashi']}</td><td dir="rtl">{word['rashi']}</td></tr>\n"""
     html += "</table>\n"
     return html
+
+def render(template, filename=None, **args):
+    root = os.path.dirname(os.path.abspath(__file__))
+    templates_dir = os.path.join(root, "templates")
+    env = Environment(loader=FileSystemLoader(templates_dir), autoescape=True)
+    html_template = env.get_template(template)
+    html = html_template.render(**args)
+
+    full_path = os.path.join(html_path, filename)
+    dir_path = os.path.dirname(full_path)
+    os.makedirs(dir_path, exist_ok=True)
+
+    with open(full_path, "w") as fh:
+        fh.write(html)
+
 
 main()
